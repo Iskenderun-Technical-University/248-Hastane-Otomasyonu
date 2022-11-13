a25e5f62de983129a9fa7605907de9da63d8e71a
Nurullah <uygulama04@gmail.com>
2022-11-13 18:06:53 +0300

    .....

--- 248-mucteba-hastahane_otomasyonu/248-mucteba-hastahane_otomasyonu/Source.cpp
+++ 248-mucteba-hastahane_otomasyonu/248-mucteba-hastahane_otomasyonu/Source.cpp
@@ -5,10 +5,23 @@
 
 using namespace std;
 
+void color(int color)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+void gotoxy(int x, int y)
+{
+	COORD c;
+	c.X = x;
+	c.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+
 class Adres {
 	string mahalle, sokak, ilcee, tel_no;
 	int apt_no;
-
+public:
 	Adres(string mh, string sk, string ilce, int apt, string tel) {
 		mahalle = mh;
 		sokak = sk;
@@ -21,6 +34,7 @@ class Adres {
 		cout << " " << mahalle << " " << sokak << " " << ilcee << " " << apt_no << " " << tel_no << endl;
 	}
 };
+
 int main() {
 	setlocale(LC_ALL, "Turkish");
 	system("title Ýskenderun Devlet Hastanesi                                                                                                          ");
